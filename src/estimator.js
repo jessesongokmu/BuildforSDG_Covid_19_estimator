8feaf96df2989bda148df88d43fbfec031168a49
jessesongok <jessesongok@gmail.com>
2020-04-13 20:29:13 +0300

    Attempting Challenge One -> currentlyInfected -fixed linting errors -> InfectionsByReportedTime -> Refactored to add functions that Compute time depending on the PeriodTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,35 @@
+const InfectionsByReportedTime = (data, CurrentlyInfected) => {
+  let timePeriod = '';
+  if (data.periodType.toLowerCase() === 'days') {
+    timePeriod = Math.trunc((data.timeToElapse * 1));
+  }
+  if (data.periodType.toLowerCase() === 'weeks') {
+    timePeriod = Math.trunc((data.timeToElapse * 7));
+  }
+  if (data.periodType.toLowerCase() === 'months') {
+    timePeriod = Math.trunc((data.timeToElapse * 30));
+  }
+  return CurrentlyInfected * ((2 * (timePeriod / 3)));
+};
+
+
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  // Challenge Computation
+  // Challenge One Computation
   const ImpactCurrentlyInfected = input.reportedCases * 10;
   const SevereImpactCurrentlyInfected = input.reportedCases * 50;
 
-  const ImpactInfectionsByReportedTime = ImpactCurrentlyInfected * (
-    2 * (Math.floor(input.timeToElapse / 3))
-  );
-  const SevereInfectionsByReportedTime = SevereImpactCurrentlyInfected * (
-    2 * (Math.floor(input.timeToElapse / 3))
-  );
   return {
     data: input,
     impact: {
       // challenge One
       currentlyInfected: ImpactCurrentlyInfected,
-      infectionsByReportedTime: ImpactInfectionsByReportedTime
+      infectionsByReportedTime: InfectionsByReportedTime(input, ImpactCurrentlyInfected)
     },
     severeImpact: {
       // challenge One
       currentlyInfected: SevereImpactCurrentlyInfected,
-      infectionsByReportedTime: SevereInfectionsByReportedTime
+      infectionsByReportedTime: InfectionsByReportedTime(input, SevereImpactCurrentlyInfected)
     }
   };
 };
