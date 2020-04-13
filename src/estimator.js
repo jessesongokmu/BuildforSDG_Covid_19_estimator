0dc9e2f45757d1e95d9534a494182f3fd0164579
jessesongok <jessesongok@gmail.com>
2020-04-13 19:45:44 +0300

    Attempting Challenge One -> currentlyInfected -fixed linting errors -> InfectionsByReportedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,26 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  // Challenge Computation
+  const ImpactCurrentlyInfected = input.reportedCases * 10;
+  const SevereImpactCurrentlyInfected = input.reportedCases * 50;
 
+  const ImpactInfectionsByReportedTime = ImpactCurrentlyInfected * (
+    2 * (Math.floor(input.timeToElapse / 3))
+  );
+  const SevereInfectionsByReportedTime = SevereImpactCurrentlyInfected * (
+    2 * (Math.floor(input.timeToElapse / 3))
+  );
   return {
     data: input,
     impact: {
       // challenge One
-      currentlyInfected: input.reportedCases * 10
+      currentlyInfected: ImpactCurrentlyInfected,
+      infectionsByReportedTime: ImpactInfectionsByReportedTime
     },
     severeImpact: {
       // challenge One
-      currentlyInfected: input.reportedCases * 50
+      currentlyInfected: SevereImpactCurrentlyInfected,
+      infectionsByReportedTime: SevereInfectionsByReportedTime
     }
   };
 };
