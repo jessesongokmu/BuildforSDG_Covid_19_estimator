579f7b0fdefb4807263de3da3f328c593eefae02
jessesongok <jessesongok@gmail.com>
2020-04-13 19:16:57 +0300

    Attempting Challenge One -> currentlyInfected

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+
+  return {
+    data: input,
+    impact: {
+      //challenge One
+      currentlyInfected: input.reportedCases * 10,
+    },
+    severeImpact: {
+      //challenge One
+      currentlyInfected : input.reportedCases * 50
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
