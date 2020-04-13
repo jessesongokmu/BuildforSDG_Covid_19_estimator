796b2fae5a87cff66536e3f499f2e8ddd7c72925
jessesongok <jessesongok@gmail.com>
2020-04-13 22:39:18 +0300

    Attempt Challange Two -> SevereCasesByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,4 @@
-const InfectionsByRequestedTime = (data, CurrentlyInfected, timePeriod) => {
-  Math.trunc(CurrentlyInfected * (2 ** (Math.trunc(timePeriod / 3))));
-};
-const severeCaseByRequestedTime = (data, CurrentlyInfected, time) => {
-  const severeCases = Math.trunc(CurrentlyInfected * (2 ** (Math.trunc(time / 3))));
-  return Math.trunc(severeCases * 0.15);
-};
-
-const covid19ImpactEstimator = (data) => {
-  const input = data;
-  // Challenge One Computation.
-  const ImpactCurrentlyInfected = data.reportedCases * 10;
-  const SevereCurrentlyInfected = data.reportedCases * 50;
-
+const InfectionsByRequestedTime = (data, CurrentlyInfected) => {
   let time = '';
   if (data.periodType.toLowerCase() === 'days') {
     time = Math.trunc((data.timeToElapse * 1));
@@ -23,21 +10,35 @@ const covid19ImpactEstimator = (data) => {
     time = Math.trunc((data.timeToElapse * 30));
   }
 
+  return Math.trunc(CurrentlyInfected * (2 ** (Math.trunc(time / 3))));
+};
+// const severeCaseByRequestedTime = () => {
+//   return Math.trunc(InfectionsByRequestedTime * 0.15);
+//   // return InfectionsByRequestedTime * 0.15;
+// };
+
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  // Challenge One Computation.
+  const ImpactCurrentlyInfected = data.reportedCases * 10;
+  const SevereCurrentlyInfected = data.reportedCases * 50;
+
+
   return {
     data: input,
     impact: {
       // challenge One
       currentlyInfected: ImpactCurrentlyInfected,
-      infectionsByRequestedTime: InfectionsByRequestedTime(input, ImpactCurrentlyInfected, time),
+      infectionsByRequestedTime: InfectionsByRequestedTime(input, ImpactCurrentlyInfected)
       // challenge Two
-      severeCasesByRequestedTime: severeCaseByRequestedTime(input, ImpactCurrentlyInfected, time)
+      // severeCasesByRequestedTime: severeCaseByRequestedTime
     },
     severeImpact: {
       // challenge One
       currentlyInfected: SevereCurrentlyInfected,
-      infectionsByRequestedTime: InfectionsByRequestedTime(input, SevereCurrentlyInfected, time),
+      infectionsByRequestedTime: InfectionsByRequestedTime(input, SevereCurrentlyInfected)
       // challenge Two
-      severeCasesByRequestedTime: severeCaseByRequestedTime(input, SevereCurrentlyInfected, time)
+      // severeCasesByRequestedTime: severeCaseByRequestedTime(input, SevereCurrentlyInfected)
     }
   };
 };
